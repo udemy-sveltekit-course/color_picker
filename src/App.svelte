b088dbdcbb0727e4edf0d697ca7f9ae872c930a1
phptuts <glaserpower@gmail.com>
2022-12-22 22:26:39 -0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Slider from './Slider.svelte';
+  let redColor = 100;
+  let greenColor = 0;
+  let blueColor = 0;
+</script>
+
+<div class="row">
+  <div class="col">
+    <h1>Color Picker</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="column">
+    <form>
+      <Slider colorName="Red" bind:colorValue={redColor} />
+      <Slider colorName="Green" bind:colorValue={greenColor} />
+      <Slider colorName="Blue" bind:colorValue={blueColor} />
+    </form>
+  </div>
+</div>
+<div class="row">
+  <div class="column">
+    <div
+      id="color"
+      style="background-color: rgb({redColor}, {greenColor}, {blueColor})"
+    />
+  </div>
+</div>
+<div class="row">
+  <div class="column">
+    <h2>rgb({redColor}, {greenColor}, {blueColor})</h2>
+  </div>
+</div>
+
+<style>
+  #color {
+    height: 300px;
+    border: dashed 5px;
+  }
+  h2 {
+    text-align: center;
+    margin-top: 20px;
+  }
+</style>
